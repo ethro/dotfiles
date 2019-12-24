5f60290fe184cd2408a5869c4af512e691842d77
ethro <josh.a.boren@gmail.com>
2019-12-24 12:28:56 -0500

    add setup script

---
+++ .dotSetup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+git clone --bare https://github.com/ethro/dotfiles.git $HOME/.dotfiles
+
+function dots {
+   /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME $@
+}
+
+mkdir -p .dotfile-backup
+dots checkout
+
+if [ $? = 0 ]; then
+   echo "checkout out dotfiles."
+else
+   echo "Backing up pre-existing dot files."
+   dots checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfile-backup/{}
+fi
+
+dots checkout
+
+dots config status.showUntrackedFiles no
+
