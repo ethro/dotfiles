e9bb8412dff56b6a3ace68b3195f9847f1740a77
Joshua Boren <josh.a.boren@gmail.com>
2019-12-27 18:34:08 -0800

    fix PS1 on msys

--- .bashrc.d/msys.sh
+++ .bashrc.d/msys.sh
@@ -1,6 +1,7 @@
 if [[ "$OSTYPE" = "msys" ]]; then
-   PS1='\[\033\]0$TITLEPREFIX:${PWD//[^[:ascii:]]/?}\007\]' # set window title
-   PS1="$PS1"'\[\-33[32m\]'   # change to green
+   #PS1='\[\033\]0;$TITLEPREFIX:${PWD//[^[:ascii:]]/?}\007\]' # set window title
+   PS1=''
+   PS1="$PS1"'\[\033[32m\]'   # change to green
    PS1="$PS1"'\u@\h '         # user@host<space>
    PS1="$PS1"'\[\033[33m\]'   # change to brownish yellow
    PS1="$PS1"'\W'             # current working directory
@@ -16,7 +17,7 @@ if [[ "$OSTYPE" = "msys" ]]; then
       then
          . "$COMPLETION_PATH/git-completion.bash"
          . "$COMPLETION_PATH/git-prompt.sh"
-         PS1="$PS1"'\[\033[35m\]'   # change color to cyan
+         PS1="$PS1"'\[\033[36m\]'   # change color to cyan
          PS1="$PS1"'`__git_ps1`'    # bash function
          PS1="$PS1"'\[\033[0m\]'    # change color to white
       fi
