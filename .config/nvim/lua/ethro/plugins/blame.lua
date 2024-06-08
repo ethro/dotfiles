625ef814450b4559e8ef6a86645fcd2d24df5bfb
a00 <a00@mail.mail>
2024-06-08 13:02:19 -0600

    more updates

---
+++ .config/nvim/lua/ethro/plugins/blame.lua
@@ -0,0 +1,13 @@
+local M = {
+  {
+    -- https://github.com/FabijanZulj/blame.nvim
+    -- git blame with modes:
+    -- virtual - draws the blame info on the far right of the buffer
+    -- window  - opens a new buffer to the left and shows the blame info
+    "FabijanZulj/blame.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+}
+
+return M
--- .config/nvim/lua/ethro/plugins/experiment.lua
+++ .config/nvim/lua/ethro/plugins/experiment.lua
@@ -4,29 +4,9 @@ local M = {
     -- Automagical editing and creation of snippets.
     "chrisgrieser/nvim-scissors",
   },
-  {
-    -- https://github.com/FabijanZulj/blame.nvim
-    -- git blame with modes:
-    -- virtual - draws the blame info on the far right of the buffer
-    -- window  - opens a new buffer to the left and shows the blame info
-    "FabijanZulj/blame.nvim",
-    event = "VeryLazy",
-  },
-  {
-    "FabijanZulj/blame.nvim",
-    config = function()
-      require("blame").setup()
-    end,
-  },
 }
 
 --[[
---blame.nvim
---Comment.nvim
---goto-preview
---hex.nvim
---kanagawa.nvim
---nvim-scissors
 --toggle-lsp-diagnostics
 --]]
 
